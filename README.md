f24768493e9d501035800039147075e5eb7d2d39
Rahul Bhatia <rbhatia46@gmail.com>
2020-08-20 17:07:07 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+Spatio-temporal algorithm implemented using Python.
+
+##  Working of the algorithm
+
+Algorithm is quite similar to the usual DBSCAN algorithm, with an addition to incorporate the temporal information, if any.
+
+There are primarily 3 parameters in this implementation - 
+
+* eps1/spatial threshold - This is similar to epsilon in DBSCAN
+* eps2/temporal threshold
+* min_neighbors - This is similar to MinPts in DBSCAN.
+
+If the time difference between successive points is within eps2 AND the distance between successive points is less than eps1, they belong to same cluster.
+
+There are no defined rules for choosing the values of these parameters, selection of these depend entirely on specific application and use-case.
+
+**Notice I have used AND in above statement, i.e. both conditions must be followed, for points to belong to the same cluster.**
+
+An intuitive explanation can be understood by the image below - 
+
+
+
+![](./static/working.png)
+
+If you need some more clariications, here are some [slides](http://www.bios.unc.edu/~dzeng/BIOS740/Qin_Bios740.pdf) by Department of Statistics and Operations Research, The University of North Carolina
+
+## An Important Note before usage - 
+If you are using this for GPS data, ensure that you convert the CRS of latitude, longitude to UTM.(you can use this [library](https://pypi.org/project/utm/) to do that), that is because this algorithm needs to calculate multiple distances between points, it optimizes by assuming latitude and longitude columns in UTM projection(since Euclidean distance is simpler and faster to calculate than haversine).
+
+## Usage
+
+```python
+import st_dbscan
+st_dbscan = st_dbscan.STDBSCAN(spatial_threshold=50, temporal_threshold=700,
+                         min_neighbors=2)
+st_dbscan.fit_transform(data,col_lat='latitude',col_lon='longitude',col_time='ts')
+```
+* As mentioned, above assumes your latitude and longitude are in UTM Projection.
+* Invoking .fit_transform() returns a Pandas Series with cluster labels
+
+
+For detailed explanation, you can refer the following paper - 
+```
+Birant, D. and Kut, A. (2007). St-dbscan: An algorithm for clustering 
+spatial–temporal data. Data & Knowledge Engineering, 60(1):208 – 221. 
+Intelligent Data Mining.
+```
---
+++ st_dbscan.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
+import pyproj
+
+
+class STDBSCAN(object):
+
+    def __init__(self, spatial_threshold=200.0, temporal_threshold=180.0,
+                 min_neighbors=10):
+        """
+        :spatial_threshold: Maximum spatial distance value (meters) for points to be in the same cluster
+        :temporal_threshold: Maximum temporal difference value (seconds) for points to be in the same cluster
+        :min_neighbors: Minimum number of points within Eps1 and Eps2;
+        """
+        self.spatial_threshold = spatial_threshold
+        self.temporal_threshold = temporal_threshold
+        self.min_neighbors = min_neighbors
+
+    def _retrieve_neighbors(self, index_center, matrix):
+
+        center_point = matrix[index_center, :]
+        min_time = center_point[2] - timedelta(seconds=self.temporal_threshold)
+        max_time = center_point[2] + timedelta(seconds=self.temporal_threshold)
+        matrix = matrix[(matrix[:, 2] >= min_time) &
+                        (matrix[:, 2] <= max_time), :]
+        temp = (matrix[:, 0]-center_point[0])*(matrix[:, 0]-center_point[0]) + \
+            (matrix[:, 1]-center_point[1])*(matrix[:, 1]-center_point[1])
+        neigborhood = matrix[temp <= (
+            self.spatial_threshold*self.spatial_threshold), 4].tolist()
+        neigborhood.remove(index_center)
+
+        return neigborhood
+
+    def fit_transform(self, df, col_lat, col_lon, col_time,
+                      col_cluster='cluster'):
+        """
+        :df: DataFrame input
+        :col_lat: Latitude column name
+        :col_lon:  Longitude column name
+        :col_time: Timestamp column name
+        :col_cluster: Alias for predicted cluster
+        """
+        cluster_label = 0
+        noise = -1
+        unmarked = 999999
+        stack = []
+
+        df = df[[col_lon, col_lat, col_time]]
+        df[col_cluster] = unmarked
+        df['index'] = range(df.shape[0])
+        matrix = df.values
+        df.drop(['index'], inplace=True, axis=1)
+
+        for index in range(matrix.shape[0]):
+            if matrix[index, 3] == unmarked:
+                neighborhood = self._retrieve_neighbors(index, matrix)
+
+                if len(neighborhood) < self.min_neighbors:
+                    matrix[index, 3] = noise
+                else:  
+                    cluster_label += 1
+                    matrix[index, 3] = cluster_label
+
+                    for neig_index in neighborhood:
+                        matrix[neig_index, 3] = cluster_label
+                        stack.append(neig_index)  
+
+                    while len(stack) > 0:
+                        current_point_index = stack.pop()
+                        new_neighborhood = \
+                            self._retrieve_neighbors(current_point_index,
+                                                     matrix)
+
+                        if len(new_neighborhood) >= self.min_neighbors:
+                            for neig_index in new_neighborhood:
+                                neig_cluster = matrix[neig_index, 3]
+                                if any([neig_cluster == noise,
+                                        neig_cluster == unmarked]):
+                                    matrix[neig_index, 3] = cluster_label
+                                    stack.append(neig_index)
+
+        df[col_cluster] = matrix[:, 3]
+        return df.cluster
